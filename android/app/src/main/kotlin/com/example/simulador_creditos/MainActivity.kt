fab63fa84c22633b8c3ddc79d35e501d7fe02355
Jose Contreras <josedanielcd@hotmail.com>
2023-08-15 20:26:31 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/simulador_creditos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simulador_creditos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+// PICTURES
+
+import 'package:flutter/material.dart';
+
+abstract class Pictures {
+  static String onBoarding1 = "photos/onboarding_1";
+  static String onBoarding2 = "photos/onboarding_2";
+}
+
+abstract class AppColors {
+  static Color principalColor = const Color.fromRGBO(84, 40, 241, 1);
+  static Color secondaryColor = Colors.transparent;
+}
+ 
